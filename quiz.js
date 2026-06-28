40b9cbf45cf3bcc7d94f0b0f686bde84ac356523
Jaely <jaelynsmith223@gmail.com>
2026-06-28 01:23:29 -0400

    Finally got the question, button, and text in the button to change when you click the correct answer.

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-var score = 0;
 var correctAnswerScore = 0;
 var wrongAnswerScore = 0;
 var questionCount = 0;
@@ -15,9 +14,11 @@ var q4a1 = document.getElementById("q4a1");
 var q4a2 = document.getElementById("q4a2");
 var q5a1 = document.getElementById("q5a1");
 var q5a2 = document.getElementById("q5a2");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
 
 const quizData =[{
-    question:"Who did Chihaya end up with",
+    question:"Who did Chihaya end up with?",
     options: ["Arata", "Taichi"],
     answer:"Taichi",
 
@@ -26,40 +27,61 @@ const quizData =[{
     question: "What does unmei-san translate to?",
     options:["A battle of fates", "Having really bad luck"],
     answer:"A battle of fates",
+},
+{
+    question: "What are dead cards in karuta called?",
+    options: ["karafuda", "torifuda"],
+    answer: "karafuda",
+},
+{
+    question: "Who is the author of Chihayafuru?",
+    options:["Yumi Tamura", "Yuki Suegetsu"],
+    answer: "Yuki Suegetsu",
+},
+{
+    question: "How many cards does each player get at the start?",
+    options: ["25", "30"],
+    answer:"25",
 }
-
 ]
 
 function wrongAnswer(){
-    wrongAnswerScore +=1;
-    questionCount +=1;
+    wrongAnswerScore ++;
 }
 
 function correctAnswer(){
-    correctAnswerScore += 1;
-    questionCount +=1;
+    correctAnswerScore ++;
+    questionCount ++;
 }
+
+
 
 function showQuestion(){
     const question =  quizData[questionCount];
     questionElement.innerText = question.question;
+    answer1.innerText = question.options[0]
+    answer2.innerText = question.options[1]
     question.options.forEach(options => {
-         const button = document.createElement("button");
-        button.innerText = options;
-        answer.appendChild(button);
-        button.addEventListener("click", function(){
-            isAnswerCorrect(button.innerText);
-            })
-    })
-}
+        answer1.addEventListener("click", () => {
+            isAnswerCorrect(answer1.innerText);
+            showQuestion();
+     }
+    )})
+    question.options.forEach(options => {
+        answer2.addEventListener("click", () => {
+            isAnswerCorrect(answer2.innerText);
+            showQuestion();
+        })})
+    }
+
 
 
 function isAnswerCorrect(selectedAnswer){
-    if(selectedAnswer === quizData[questionCount].answer){
-        correctAnswer()
+    if(selectedAnswer == quizData[questionCount].answer){
+        correctAnswer();
     } else{
         wrongAnswer();
     }
-}
 
-showQuestion();
+}
+ showQuestion();
